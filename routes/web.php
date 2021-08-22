e30d9bdb7c425892c13dfbcb46f03e4427a053c9
hamabehayato <be_dash1122@icloud.com>
2021-08-22 17:46:41 +0900

    login, logout 機能の追加

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 //ユーザ登録ページ
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+//ログイン・アウト処理
+//ログインページ表示
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+//フォームの入力内容を送信
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+//ログアウト
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
