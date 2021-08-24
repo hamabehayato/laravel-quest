7d3ac9ad76edefcf58572624d6363fdabd380f68
hamabehayato <be_dash1122@icloud.com>
2021-08-24 09:45:38 +0900

    publicにアクセスしても表示されない問題解消

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-//ログイン・アウト処理
+//ログイン・アウト
 //ログインページ表示
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-//フォームの入力内容を送信
+//フォームに入力した情報を送信
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 //ログアウト
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
