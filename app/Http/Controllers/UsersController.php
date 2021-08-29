28776ebecfb7ed37cf1e0c1afb2e5a451aea116a
hamabehayato <be_dash1122@icloud.com>
2021-08-29 13:06:42 +0900

    ユーザー詳細の機能追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,7 @@ use App\User;
 
 class UsersController extends Controller
 {
+    //indexアクション
     public function index()
     {
         //orderBy('id', 'desc') すべてのユーザをＩＤが新しい順（降順）に並び替える
@@ -17,5 +18,24 @@ class UsersController extends Controller
         return view('welcome', [
             'users' => $users,
         ]);
+    }
+
+    //showアクション(ユーザー詳細)
+    public function show($id)
+    {   
+        //idが一致するユーザーを検索
+        $user = User::find($id);
+        //user_idとidが一致するmovieを検索
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+
+        //$dataにcontroller.phpよりcounts($user)の戻り値を代入
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
